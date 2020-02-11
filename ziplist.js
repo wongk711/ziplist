8a67fd8578300d570cdc50667e667ccbb875d243
Kameron Wong <wongk711@hawaii.edu>
2020-02-11 00:16:30 -1000

    npm install

--- ziplist.js
+++ ziplist.js
@@ -1,18 +1,17 @@
-const testList1 = ['a','b','c'];
-const testList2 = [1,2,3];
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
 
-function ziplist( list1, list2){
+function ziplist(list1, list2) {
   const resultList = [];
-  for (let i =0 ; i < list1.length; i++){
+  for (let i = 0; i < list1.length; i++) {
     resultList.push(list1[i], list2[i]);
   }
   return resultList;
 }
 
-function ziplistSimple(list1, list2){
+function ziplistSimple(list1, list2) {
   return _.flatten(_.zip(list1, list2));
 }
 
 console.log(ziplist(testList1, testList2));
-console.log(ziplistSimple(testList1, testList 2));
-
+console.log(ziplistSimple(testList1, testList2));
